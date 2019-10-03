d017325de9d23eb92837819b2a62b2a14ccec656
Anastasiya-M <55109262+Anastasiya-M@users.noreply.github.com>
2019-10-03 20:20:29 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -35,10 +35,32 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    '': ' '
 };
 
 function decode(expr) {
-    // write your solution here
+    let bufferArray = [];
+    let i = 0;
+    while(i < expr.length) {
+        bufferArray.push("" + Number(expr.substr(i, 10)));
+        i += 10;
+    }
+
+    let stringArray = bufferArray.map(function(item) {
+        let resultString = '';
+        for(let i = 0; i < item.length; i += 2) {
+            if(item[i] == 1 && item[i + 1] == 0) resultString += '.';
+            if(item[i] == 1 && item[i + 1] == 1) resultString += '-';
+        }
+        
+        return resultString;
+    });
+
+    let finalArray = stringArray.map(function(item) {
+        return MORSE_TABLE[item];
+    });
+
+    return finalArray.join('');
 }
 
 module.exports = {
